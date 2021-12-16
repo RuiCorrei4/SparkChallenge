97f68d4107549bf8f62bb3cb2a350704bb550e5e
ruicorrei4 <ruicorrei4@gmail.com>
2021-12-16 03:53:47 +0000

    parte 2

--- src/main/scala/com/spark/App.scala
+++ src/main/scala/com/spark/App.scala
@@ -7,6 +7,7 @@ import org.apache.spark.sql.functions._
 
 
 
+
 /**
  * @author ${user.name}
  */
@@ -19,10 +20,20 @@ object App {
     val spark = SparkSession.builder()
       .master( master = "local[*]")
       .getOrCreate()
-    //import csv
-    val df_0 = spark.read.options(Map("inferSchema"->"true","header"->"true")).csv("/home/ruicorreia/Documents/Spark/google-play-store-apps/googleplaystore_user_reviews.csv")
-    //part 1 
-    val df_1 = df_0.select("App","Sentiment_Polarity").groupBy("App").agg(avg("Sentiment_Polarity").cast(DoubleType).as("Average_Sentiment_Polarity")).na.fill(0)
+    //parte 1
+    val df_1 = spark.read.options(Map("inferSchema"->"true","header"->"true")).csv("/home/ruicorreia/Documents/Spark/google-play-store-apps/googleplaystore_user_reviews.csv")
+      .select("App","Sentiment_Polarity")
+      .groupBy("App")
+      .agg(avg("Sentiment_Polarity")
+        .cast(DoubleType).as("Average_Sentiment_Polarity"))
+      .na.fill(0)
+      df_1.show()
+    //parte 2 not null e not NaN for style points 
+    val df_2 = spark.read.options(Map("inferSchema"->"true","header"->"true"))
+      .csv("/home/ruicorreia/Documents/Spark/google-play-store-apps/googleplaystore.csv") 
+      .filter(org.apache.spark.sql.functions.col("Rating").cast("double") >=4.0 && org.apache.spark.sql.functions.col("Rating").isNotNull && !org.apache.spark.sql.functions.col("Rating").isNaN)
+      .orderBy(org.apache.spark.sql.functions.col("Rating").desc)
+    df_2.show()
+    df_2.write.options(Map("header"->"true", "delimiter"->"§")).csv("best_apps.csv")
   }
-
 }
